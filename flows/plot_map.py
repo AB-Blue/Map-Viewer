a1f21e0327c14243e369f1c11c26f7725bbc6e3a
Amir Abbas Babasafari <38904603+AB-Blue@users.noreply.github.com>
2025-03-21 12:17:53 +0100

    Update plot_map.py

--- flows/plot_map.py
+++ flows/plot_map.py
@@ -8,7 +8,7 @@ def run():
 
     # Dropdown to select map display mode
     display_mode = dropdown(
-        "display_mode",
+        "visualization_mode",
         options=["2D", "3D"],
         value="2D",  # Default value
         label="Select Map Display Mode"
