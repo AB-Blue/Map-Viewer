d84423b66f5fda0d9b2374e4e616bcdeda660c82
Amir Abbas Babasafari <38904603+AB-Blue@users.noreply.github.com>
2025-03-21 12:41:45 +0100

    Update plot_map.py

--- flows/plot_map.py
+++ flows/plot_map.py
@@ -39,6 +39,7 @@ def run():
     )
     
     # File output to save the generated map
+    print('ab', f"mode{visualization_mode}")
     output_file = file_output("output_map", "output_map.png" if visualization_mode == "2D" else "output_map.html")
 
     # Log the input parameters
